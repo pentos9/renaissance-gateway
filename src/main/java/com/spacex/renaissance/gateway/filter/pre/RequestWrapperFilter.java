fb4775952c9d551b44149a55d9cbfac61a558508
lucas ley <ayaaa168@163.com>
2019-03-16 18:00:34 +0800

    feat(RequestWrapperFilter):add request wrapper filter

--- src/main/java/com/spacex/renaissance/gateway/filter/pre/RequestWrapperFilter.java
+++ src/main/java/com/spacex/renaissance/gateway/filter/pre/RequestWrapperFilter.java
@@ -1,6 +1,11 @@
 package com.spacex.renaissance.gateway.filter.pre;
 
 import com.spacex.renaissance.gateway.filter.RenaissanceFilter;
+import com.spacex.renaissance.gateway.http.RequestContext;
+import com.spacex.renaissance.gateway.http.RequestEntity;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 public class RequestWrapperFilter extends RenaissanceFilter {
     public String filterType() {
@@ -12,6 +17,28 @@ public class RequestWrapperFilter extends RenaissanceFilter {
     }
 
     public void run() {
+        String rootURL = "http://localhost:9090";
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest servletRequest = requestContext.getRequest();
 
+        String targetURL = rootURL + servletRequest.getRequestURI();
+        RequestEntity<byte[]> requestEntity = null;
+        try {
+            requestEntity = createRequestEntity(servletRequest, targetURL);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        requestContext.setRequestEntity(requestEntity);
+
+    }
+
+    private RequestEntity<byte[]> createRequestEntity(HttpServletRequest servletRequest, String targetURL) {
+        String requestMethod = servletRequest.getMethod();
+        return null;
+    }
+
+    private Map<String, String> createRequestHeaders(HttpServletRequest request) {
+        return null;
     }
 }
