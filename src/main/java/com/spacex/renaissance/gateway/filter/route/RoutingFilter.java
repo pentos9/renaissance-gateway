8077b91c194b919986f54903a13d69e8e5adfe25
lucas ley <ayaaa168@163.com>
2019-03-17 22:22:58 +0800

    feat(RouteFilter):add route filter

--- src/main/java/com/spacex/renaissance/gateway/filter/route/RoutingFilter.java
+++ src/main/java/com/spacex/renaissance/gateway/filter/route/RoutingFilter.java
@@ -1,6 +1,8 @@
 package com.spacex.renaissance.gateway.filter.route;
 
 import com.spacex.renaissance.gateway.filter.RenaissanceFilter;
+import com.spacex.renaissance.gateway.http.RequestContext;
+import com.spacex.renaissance.gateway.http.RequestEntity;
 
 public class RoutingFilter extends RenaissanceFilter {
     public String filterType() {
@@ -12,6 +14,10 @@ public class RoutingFilter extends RenaissanceFilter {
     }
 
     public void run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        RequestEntity requestEntity = ctx.getRequestEntity();
+        // route logic
 
+        //ctx.setResponseEntity(responseEntity);
     }
 }
